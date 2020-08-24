5b31269d91bfc837e244bb9bde53ebe495940701
hdseo <terranpush@naver.com>
2020-08-24 12:05:18 +0900

    Updates

---
+++ precache-manifest.9b844cbd2b9fdf30b2743385e628902b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab6d3c654f30ca2e9dc7d1c76fbdfd5",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "6b355dff7b1cc204ea17",
+    "url": "/todo_list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "92e920d89dc327fec203",
+    "url": "/todo_list/static/js/2.094f33cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo_list/static/js/2.094f33cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b355dff7b1cc204ea17",
+    "url": "/todo_list/static/js/main.cc738666.chunk.js"
+  },
+  {
+    "revision": "cf74516e357f9a0d23f8",
+    "url": "/todo_list/static/js/runtime-main.7a3e04cd.js"
+  }
+]);
